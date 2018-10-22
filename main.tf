1e01b84a6ea9b37d0016604abd7e23dacc876f49
Nesbitt <Hans.Nesbitt@PacificLife.com>
2018-10-22 11:56:44 -0700

    updated

--- main.tf
+++ main.tf
@@ -1,14 +1,18 @@
-provider "aws" {
-  version = "~> 1.16"
+terraform {
+  required_version = ">= 0.11.0"
 }
 
-
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = ""
+  version = "~> 1.5"
 }
 
-resource "aws_instance" "web" {
-  # ...
+module "server" {
+  source = "./server"
+
+  num_webs     = "${var.num_webs}"
+  identity     = "${var.identity}"
+  ami          = "${var.ami}"
+  ingress_cidr = "${var.ingress_cidr}"
+  public_key   = "${var.public_key}"
+  private_key  = "${var.private_key}"
 }
